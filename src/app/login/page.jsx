71a0b2d454c0268c2923c413a48c6b4a53167ebb
Phong-Anan Saengchuapor <61737426+PhongAnan-Golf@users.noreply.github.com>
2024-05-17 11:50:48 +0700

    Change text

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -44,16 +44,36 @@ function LoginPage() {
             <h3 className='text-3xl'>Login</h3>
             <hr className='my-3' />
             <form onSubmit={handleSubmit}>
-              {error && <div className='bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2'>{error}</div>}
-              <input type='text' onChange={(e) => setEmail(e.target.value)} className='w-full bg-gray-200 border py-2 px-3 rounded text-lg my-3' placeholder='Enter your email' />
-              <input type='pssword' onChange={(e) => setPassword(e.target.value)} className='w-full bg-gray-200 border py-2 px-3 rounded text-lg my-3' placeholder='Enter your password' />
-              <button className='bg-green-500 text-white border py-2 px-3 rounded text-lg my-2' type='submit'>
+              {error && (
+                <div className='bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2'>
+                  {error}
+                </div>
+              )}
+              <input
+                type='text'
+                onChange={(e) => setEmail(e.target.value)}
+                className='w-full bg-gray-200 border py-2 px-3 rounded text-lg my-3'
+                placeholder='Enter your email'
+              />
+              <input
+                type='password'
+                onChange={(e) => setPassword(e.target.value)}
+                className='w-full bg-gray-200 border py-2 px-3 rounded text-lg my-3'
+                placeholder='Enter your password'
+              />
+              <button
+                className='bg-green-500 text-white border py-2 px-3 rounded text-lg my-2'
+                type='submit'
+              >
                 Sign in
               </button>
               <hr className='my-3' />
               <p>
                 Do you have an account?{' '}
-                <Link href='/register' className='text-blue-500 hover:underline'>
+                <Link
+                  href='/register'
+                  className='text-blue-500 hover:underline'
+                >
                   Register
                 </Link>
               </p>
